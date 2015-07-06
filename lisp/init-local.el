fd39949ba699601e1ef75a87895079ac665d11be
James Fletcher <m@jef.me.uk>
2015-07-06 05:56:31 +0100

    ?Disable session point memory forcommit messages.
    See https://github.com/magit/magit/issues/1954
    
    D#	lisp/init-twitter.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -24,7 +24,54 @@
 
 ;;; Code:
 
+(setq user-full-name "James Fletcher"
+      user-mail-address "m@jef.me.uk")
 
+(set-frame-font "Terminus-14")
+
+(load-theme 'wombat t)
+(blink-cursor-mode -1)
+(setq-default cursor-type 'bar)
+(set-cursor-color "#999999")
+
+;;* Org mode
+(defvar ora-org-basedir "~/priv/org/")
+
+(defun ora-org-expand (file)
+  (expand-file-name file ora-org-basedir))
+(setq org-agenda-files
+      (mapcar #'ora-org-expand '("todo.org" "gtd.org" "ent.org")))
+
+(setq diary-file (ora-org-expand "diary"))
+(setq org-agenda-include-diary t)
+
+;; org-mode wiki
+(use-package plain-org-wiki
+             :commands plain-org-wiki plain-org-wiki-helm
+             :config
+             (setq pow-directory
+                   (ora-org-expand "wiki/")))
+
+(require 'elfeed)
+(setq elfeed-feeds
+      '("http://nullprogram.com/feed/"
+        "http://emacsredux.com/atom.xml"
+        "http://emacs-fu.blogspot.com/feeds/posts/default"
+        "http://feeds.feedburner.com/GotEmacs?format=xml"
+        "http://www.wisdomandwonder.com/tag/emacs/feed"
+        "http://irreal.org/blog/?tag=emacs&feed=rss2"
+        "http://feeds.feedburner.com/tuxicity-emacs?format=xml"
+        "http://jbm.io/categories/emacs/atom.xml"
+        "https://www.blogger.com/feeds/4394570295456001999/posts/default/-/Emacs"
+        "http://dialectical-computing.de/blog/emacs.xml"
+        "http://technomancy.us/feed/atom"
+        "https://www.blogger.com/feeds/8696405790788556158/posts/default/-/emacs"
+        "http://www.randomsample.de/dru5/taxonomy/term/2/0/feed"
+        "https://www.blogger.com/feeds/4166588008280027121/posts/default/-/planetemacsen"
+        "http://bling.github.io/atom.xml"
+        "http://axisofeval.blogspot.com/feeds/posts/default"
+        "http://endlessparentheses.com/atom.xml"
+        "http://oremacs.com/atom.xml"))
 
 (provide 'init-local)
 ;;; init-local.el ends here
